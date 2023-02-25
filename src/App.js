10cead6bb4b0cf96975d7e5168e14daeb4e98fd7
Rithik <rithu0644@gmail.com>
2023-02-25 17:02:13 +0530

    Removed unwanted files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { marked } from "marked";
+import "@fontsource/quicksand"
+import "@fontsource/satisfy"
 
 function App() {
+
+  const [input, setInput] = useState(sample_text);
+  const [output, setOutput] = useState("");
+
+  const generate = () => {
+    setOutput(marked(input));
+  }
+
+  const update = (event) => {
+    setOutput(marked(event.target.value));
+    setInput(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 style={{ textAlign: 'center', fontFamily: "Satisfy" }} className="title">Markdown Previewer</h1>
+      <textarea style={{ fontFamily: "Quicksand" }} className="input" onChange={update} value={input} placeholder="Enter your markdown here" />
+
+      {
+        (output == "") ?
+
+          <center onMouseOver={generate} onTouchStart={generate}><p>Touch me...🤫</p></center> :
+
+          <div className="output"
+            dangerouslySetInnerHTML={{
+              __html: output,
+            }}
+          ></div>
+      }
+
     </div>
   );
+
 }
+
+const sample_text = `# Howdy
+
+## Rithik here 😎
+
+[GitHub Repo containing this project](https://github.com/RithikRaj64/Markdown-Previewer)
+
+[Project has been deployed in this link](https://rithikraj64.github.io/Markdown-Previewer)
+
+\`You can insert an Inline Code\`
+
+    Or you can go crazy and have a whole Code Block
+
+- You can create an Unordered list like this....
+
+1) Or if you are well organized like me use these Ordered lists...
+
+>A great man once said live life fearless and bold
+
+**And that's why I made this line Bold**`;
 
 export default App;
